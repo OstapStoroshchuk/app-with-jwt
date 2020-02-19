79e9541b77dd27ef5fcc9e19e89643fd09b1a856
Ostap Storoshchuk <ostap.storoshchuk@gmail.com>
2020-02-19 22:35:23 +0200

    Initial commit

---
+++ src/main/java/ostap/storoshchuk/app/with/jwt/AppWithJwtApplication.java
@@ -0,0 +1,13 @@
+package ostap.storoshchuk.app.with.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithJwtApplication.class, args);
+	}
+
+}
